ba65594a87b968566870fa63e15f871bfb6100a8
Hozerino <hozerboy2239@gmail.com>
2025-07-26 23:15:53 -0300

    exec time

--- src/model_layer/states/state.gd
+++ src/model_layer/states/state.gd
@@ -21,3 +21,17 @@ func update(input_data: InputData, delta: float):
 func should_transition_to(input_data: InputData) -> String:
 	return self.id
 
+### Execution time
+var _state_enter_time: float = 0.0
+func execution_time() -> float:
+	return Time.get_unix_time_from_system() - _state_enter_time
+
+func execution_already_passed(seconds: float) -> bool:
+	return execution_time() >= seconds
+
+func execution_still_before(seconds: float) -> bool:
+	return execution_time() < seconds
+
+func execution_is_between(start_time: float, end_time: float) -> bool:
+	var t: float = execution_time()
+	return t >= start_time and t <= end_time
